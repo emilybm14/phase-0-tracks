1ba3d5f4245cadd7ef999d8f1e25ce306f27948b
Emily Mejer <emilybm14@gmail.com>
2017-02-20 13:36:58 -0500

    write vampire test

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,33 +1,51 @@
-
-
-
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
-
-
 puts "what is your name?"
 full_name = gets.chomp
 
+# this is the age test 
 puts "how old are you?"
 age = gets.chomp
 
+current_year = 2017
+
 puts "what year were you born? (YYYY)"
 year_born = gets.chomp
-current_year = 2017
-years_alive = current_year - year_born
 
-if years_alive != age
-	puts "you may be a vampire"
+years_alive = current_year - year_born.to_i
+
+if years_alive == age.to_i 
+  years_alive = true
+else
+  years_alive = false
 end
 
-
-puts "our cafeteria serves garlic bread. Should we order you some?"
+# this is the garlic test
+puts "our cafeteria serves garlic bread. Should we order you some? (y/n)"
 garlic_bread = gets.chomp
 
+if garlic_bread = y.include?
+	likes_garlic = true
+else
+	likes_garlic = false
+end
 
 puts "would you like to enroll in our health care insurace? (y/n)"
 health_care = gets.chomp
+
+if health_care = y.include?
+	wants_care = true
+else
+	wants_care = false
+end
+
+if test_1 = years_alive == true && (likes_garlic == true || wants_care == true)
+	puts "Probably not a vampire."
+end
+
+if test_2 = years_alive == false && (likes_garlic == false || wants_care == false)
+	puts "Probably a vampire."
+end
+
+if test_3 = years_alive == false && likes_garlic == false && wants_care == false
+
 
 
