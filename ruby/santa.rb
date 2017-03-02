45554203b1a4e123a648722ed2e841f8e2483c07
Emily Mejer <emilybm14@gmail.com>
2017-03-02 15:23:22 -0400

    got the each to work slightly

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -39,15 +39,15 @@ end
 
 # santa = Santa.new(gender, ethnicity)
 
-puts "Testing each santa instance in the array to make sure they can speak ..."
-santa.each do |words|
-  santa.speak(3)
-end
+# puts "Testing each santa instance in the array to make sure they can speak ..."
+# santa.each do |words|
+#   santa.speak(3)
+# end
 
-puts "Testing each santa instance in the array to make sure they can eat ..."
-santa.each do |cookie|
-  santa.eat_milk_and_cookies("snickerdoodle")
-end
+# puts "Testing each santa instance in the array to make sure they can eat ..."
+# santa.each do |cookie|
+#   santa.eat_milk_and_cookies("snickerdoodle")
+# end
 
  # santa.eat_milk_and_cookies("snickerdoodle")
  # santa.speak(3)
