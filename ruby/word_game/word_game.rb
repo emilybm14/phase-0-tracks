5573caabf29f1e8fe08af3f963b7cc5cdbb264a0
Emily Mejer <emilybm14@gmail.com>
2017-03-04 08:31:09 -0400

    add word game folder

---
+++ ruby/word_game/word_game.rb
@@ -0,0 +1,24 @@
+# A Game Class
+
+# Program asks user1 for a word
+# User1 enters a word
+# The word gets stored in an array 
+# User2 is prompted to start guessing letters one at a time
+# User2 enters letter
+# Game checks if letter is in word
+# If letter is in word it is placed 
+# Current state of the word is returned
+# User2 is prompted to enter another letter
+# This repeats until all spaces are filled
+# Game ends
+Business logic 
+class Word_game
+	attr_reader :word_anwer
+	attr accessor :letter_guess
+def 
+
+end
+# Start new instance 
+game = Word_game.new
+
+p game
