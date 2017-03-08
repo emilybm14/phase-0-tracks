945583ed037c562ef6123ea26e8e6b66c24782bb
Emily Mejer <emilybm14@gmail.com>
2017-03-08 16:37:35 -0500

    fix syntax

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,28 +5,28 @@
 // console.log(names, colors);
 
 var car = {make:'Lamborghini', package_level: 'poor man package', color:'yellow'};
-console.log(car);
+
 function Car(make, package_level, color){
   console.log("Our new", this);
   this.make = make
   this.package_level = package_level
   this.color = color
-this.catches_on_fire = function() {
-  console.log("Bursts into Flames!!!!!");
-};
+  this.catches_on_fire = function() {
+  console.log("Bursts into Flames!!!!!"); };
 
-console.log("Car initialization Complete");
+  console.log("Car initialization Complete");
 }
 
 console.log("Lets build a car!");
+
 var car_1 = new Car("Fiat","Rich mans package","Puke green");
-console.log(car_1);
 console.log ("Car starts and ");
 car_1.catches_on_fire();
 console.log("----");
 
 console.log("Lets build another car!");
+
 var car_2 = new Car("Lexus", "Penny Pincher package", "Silver");
 console.log ("Car starts and ");
 car_1.catches_on_fire();
-console.log("----");```
+console.log("----");
