82fa5900588271d65dec1ae281c8f4a644f1c3ce
Emily Mejer <emilybm14@gmail.com>
2017-03-11 12:05:16 -0500

    update files

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,32 +1,39 @@
-// var colors = ["blue","yellow","red", "green"];
-// var names = ["Ed","Tom","Jerry", "Mike"];
-// colors.push("purple");
-// names.push("Spock");
-// console.log(names, colors);
 
-var car = {make:'Lamborghini', package_level: 'poor man package', color:'yellow'};
+var = horse new (colors, names)
+var colors = ["blue","yellow","red", "green"];
+var names = ["Ed","Tom","Jerry", "Mike"];
+colors.push("purple");
+names.push("Spock");
+console.log(names, colors);
 
-function Car(make, package_level, color){
-  console.log("Our new", this);
-  this.make = make
-  this.package_level = package_level
-  this.color = color
-  this.catches_on_fire = function() {
-  console.log("Bursts into Flames!!!!!"); };
+console.log("The horse's name is " + horse['name'] + " and it's color is " + horse['colors'] + ".");
 
-  console.log("Car initialization Complete");
-}
 
-console.log("Lets build a car!");
+// var car = {make:'Lamborghini', package_level: 'poor man package', color:'yellow'};
 
-var car_1 = new Car("Fiat","Rich mans package","Puke green");
-console.log ("Car starts and ");
-car_1.catches_on_fire();
-console.log("----");
+// function Car(make, package_level, color){
+//   console.log("Our new", this);
+//   this.make = make
+//   this.package_level = package_level
+//   this.color = color
+//   this.catches_on_fire = function() {
+//   console.log("Bursts into Flames!!!!!"); };
 
-console.log("Lets build another car!");
+//   console.log("Car initialization Complete");
+// }
 
-var car_2 = new Car("Lexus", "Penny Pincher package", "Silver");
-console.log ("Car starts and ");
-car_1.catches_on_fire();
-console.log("----");
+// console.log("Lets build a car!");
+
+// var car_1 = new Car("Fiat","Rich mans package","Puke green");
+// console.log("this is the" + car[package_level] + "which is a" + car[color] + car['make']);
+// console.log ("Car starts and ");
+// car_1.catches_on_fire();
+// console.log("----");
+
+// console.log("Lets build another car!");
+
+// var car_2 = new Car("Lexus", "Penny Pincher package", "Silver");
+// console.log
+// console.log ("Car starts and ");
+// car_1.catches_on_fire();
+// console.log("----");
