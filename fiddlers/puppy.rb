4bcff0f5160f2359e3d0ef154deb698d7ba9221f
Emily Mejer <emilybm14@gmail.com>
2017-03-23 13:46:13 -0400

    add puppy and puppy methods

---
+++ fiddlers/puppy.rb
@@ -0,0 +1,31 @@
+# How might you design a Puppy class? What sorts of characteristics and 
+# behavior might a Puppy class involve? 
+# Using a format similar to the one we used for Eyelashus Adorableus, 
+# come up with some characteristics and behavior for a Puppy class. 
+# Use comments to add your class design to
+
+
+
+
+# ____ CLASS ____
+# Puppy
+
+# ____ ATTRIBUTES/ CHARACTERISTICS ____
+# Age: <1 Year
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Tail length: varies
+# Gender: varies
+
+
+# ____ METHODS/ BEHAVIORS ____
+# Run
+# Play
+# Get trained
+# Sleeps
+# Eats
+# pees
+# poops
+# Barks
